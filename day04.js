badefd13b80b0f5fba3bd8d3286b21ee1b1af5a2
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-17 20:31:31 +0530

    changed

--- day04.js
+++ day04.js
@@ -1,9 +1,18 @@
 let a =10;
 let b= a;
-//let b = 30;
+ b = 30;
 console.log(b);
 console.log(a);
 //primitive data type : immutable means that not change the value of a variable once it is created.
 
 //non primitive data type :  mutable 
 
+//object example:
+let obj1 ={
+    id:20,
+    name:"pawan"
+
+}
+let obj2 = obj1;
+console.log(obj1);
+console.log(obj2); 
