73ce2276f6e2cc911d03b826ade9db1ff130dfea
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-17 20:23:49 +0530

    added

---
+++ day04.js
@@ -0,0 +1,9 @@
+let a =10;
+let b= a;
+//let b = 30;
+console.log(b);
+console.log(a);
+//primitive data type : immutable means that not change the value of a variable once it is created.
+
+//non primitive data type :  mutable 
+
