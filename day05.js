4744756f1885b5cfa86de2c838c048484f715773
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-18 11:22:56 +0530

    changed

--- day05.js
+++ day05.js
@@ -1,44 +1,44 @@
-// let num1 = 231;
-// let num2 = new Number(231);
-// let num3 = new Number(231);
+let num1 = 231;
+let num2 = new Number(231);
+let num3 = new Number(231);
 
-// console.log(num1==num2);
-// console.log(num2==num3);
+console.log(num1==num2);
+console.log(num2==num3);
 
-// let num =231.68;
-// console.log(num.toFixed(2));
-// console.log(num.toPrecision(4));
-// console.log(num.toExponential(2));
-// console.log(num.toString(2));
+let num =231.68;
+console.log(num.toFixed(2));
+console.log(num.toPrecision(4));
+console.log(num.toExponential(2));
+console.log(num.toString(2));
 
-//Math
+Math
 
-// console.log(Math.E);
-// // console.log(Math.PT);
-// console.log(Math.PI);
-// console.log(Math.LN10);
-// console.log(Math.random());
+console.log(Math.E);
+// console.log(Math.PT);
+console.log(Math.PI);
+console.log(Math.LN10);
+console.log(Math.random());
 
-// //floor and ceil
-// let num = 23.1;
-// console.log(Math.floor(num));
-// console.log(Math.ceil(num));
+//floor and ceil
+let num = 23.1;
+console.log(Math.floor(num));
+console.log(Math.ceil(num));
 
-//console.log(Math.floor(Math.random()*10));
-////genrate the value from 1 to 9
+console.log(Math.floor(Math.random()*10));
+//genrate the value from 1 to 9
 
-////genrate the value between min=40 , to max = 50;
-////formula 
-////console.log(Math.floor(Math.random()*(max-min+1)+min));
+//genrate the value between min=40 , to max = 50;
+//formula 
+//console.log(Math.floor(Math.random()*(max-min+1)+min));
 
-//console.log(Math.floor (Math.random()*11)+40);
+console.log(Math.floor (Math.random()*11)+40);
 
-////console.log(Math.floor(Math.random()*10)); find number from 0 to 9 and for 0 to 10 multiply by 11
+//console.log(Math.floor(Math.random()*10)); find number from 0 to 9 and for 0 to 10 multiply by 11
 
-////1-11
-//// 2-12
-////3-13
-//// console.log(Math.floor(Math.random()*(10+1)+3));
+//1-11
+// 2-12
+//3-13
+// console.log(Math.floor(Math.random()*(10+1)+3));
 
-////ludo 1-6
+//ludo 1-6
 console.log(Math.floor(Math.random()*(6-1+1)+1));
