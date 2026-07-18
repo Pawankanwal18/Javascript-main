411d3eb084f9f8226752d2faeb2c4ae2cb28010f
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-18 10:45:49 +0530

    changed

--- day05.js
+++ day05.js
@@ -1,19 +1,20 @@
-let num1 = 231;
-let num2 = new Number(231);
-let num3 = new Number(231);
+// let num1 = 231;
+// let num2 = new Number(231);
+// let num3 = new Number(231);
 
-console.log(num1==num2);
-console.log(num2==num3);
+// console.log(num1==num2);
+// console.log(num2==num3);
 
-let num =231.68;
-console.log(num.toFixed(2));
-console.log(num.toPrecision(4));
-console.log(num.toExponential(2));
-console.log(num.toString(2));
+// let num =231.68;
+// console.log(num.toFixed(2));
+// console.log(num.toPrecision(4));
+// console.log(num.toExponential(2));
+// console.log(num.toString(2));
 
 //Math
 
-console.log(Math.E);
-// console.log(Math.PT);
-console.log(Math.PI);
-console.log(Math.LN10);
+// console.log(Math.E);
+// // console.log(Math.PT);
+// console.log(Math.PI);
+// console.log(Math.LN10);
+console.log(Math.random());
