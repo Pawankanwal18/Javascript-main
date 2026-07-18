2b58580e05e5b10780472c2f996085e9bcb4f383
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-18 10:39:45 +0530

    added

--- day05.js
+++ day05.js
@@ -4,3 +4,9 @@ let num3 = new Number(231);
 
 console.log(num1==num2);
 console.log(num2==num3);
+
+let num =231.68;
+console.log(num.toFixed(2));
+console.log(num.toPrecision(4));
+console.log(num.toExponential(2));
+console.log(num.toString(2));
