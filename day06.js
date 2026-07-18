b277cca8041d0dbadcee60acd07e19246abbc33c
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-18 11:44:09 +0530

    added

--- day06.js
+++ day06.js
@@ -1,3 +1,7 @@
-const arr=[2,34,645,434, "pawan","hello"];
+const arr=[2,34,645,434,"pawan","hello"];
 console.log(arr[4]);
 console.log(arr.length);
+console.log(arr.at(5));
+//length
+const newarr = arr;
+console.log(newarr==arr);
