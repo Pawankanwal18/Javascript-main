6babf57a6e94c276e71a0b00fb790eb70b9463f8
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-18 11:48:02 +0530

    added

--- day06.js
+++ day06.js
@@ -1,7 +1,10 @@
 const arr=[2,34,645,434,"pawan","hello"];
-console.log(arr[4]);
-console.log(arr.length);
-console.log(arr.at(5));
-//length
-const newarr = arr;
-console.log(newarr==arr);
+// console.log(arr[4]);
+// console.log(arr.length);
+// console.log(arr.at(5));
+// //length
+// const newarr = structuredClone(arr);
+// console.log(newarr==arr);
+
+arr.push(30);
+console.log(arr);
