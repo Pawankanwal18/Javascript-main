5ab3662f6e83e38f065f135c61201afc7efde8f7
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-18 20:34:30 +0530

    added

--- day06.js
+++ day06.js
@@ -1,4 +1,4 @@
-const arr=[2,34,645,434,"pawan","hello"];
+const arr=[2,34,645,"pawan","hello","true",86,73];
 // console.log(arr[4]);
 // console.log(arr.length);
 // console.log(arr.at(5));
@@ -6,14 +6,21 @@ const arr=[2,34,645,434,"pawan","hello"];
 // const newarr = structuredClone(arr);
 // console.log(newarr==arr);
 
-arr.push(30);
-console.log(arr); 
+// arr.push(30);
+// console.log(arr); 
 
-//pop, pop the last element from array
-console.log(arr.pop());
-console.log(arr.pop());
+// //pop, pop the last element from array
+// console.log(arr.pop());
+// console.log(arr.pop());
 
-//shift delete eliment from start 
-console.log(arr.shift());
-arr.unshift(100);
-console.log(arr);  
+// //shift delete eliment from start 
+// console.log(arr.shift());
+// arr.unshift(100);
+// console.log(arr);  
+// console.log(arr);
+// console.log(arr.indexOf(2));
+console.log(arr);
+console.log(arr.slice(2,4));//4th index is not included 
+
+//splice
+console.log(arr.splice(2,4));//start from 2nd index and give the 4 elements
