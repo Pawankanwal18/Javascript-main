fa67aa9788d0701ea2e45615d8399c43c7caa4f9
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-19 11:22:49 +0530

    added,corrected

--- day07.js
+++ day07.js
@@ -1,12 +1,15 @@
                   //Date//
-const d = new Date();
+// const d = new Date();
 // console.log(d.toDateString());
 // console.log(d.toString());
 // console.log(d);
-console.log(d.getDate());
-console.log(d.getDay());
-console.log(d.getMonth());
-console.log(d.getFullYear());
-console.log(d.getMilliseconds());
-const now = Date.now();
-console.log(now); 
+// console.log(d.getDate());
+// console.log(d.getDay());
+// console.log(d.getMonth());
+// console.log(d.getFullYear());
+// console.log(d.getMilliseconds());
+// const now = Date.now();
+const d = new Date("2026-2-20");
+const date = new Date(2026,2,20);
+// console.log(d.toDateString());
+console.log(date.toString());
