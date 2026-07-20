bca89bd5d9493c51c72fb389d6e9475c151bf6e6
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-20 13:29:43 +0530

    added

--- day08.js
+++ day08.js
@@ -1,37 +1,53 @@
-                      //objects
+//                       //objects
 
-let obj = {
-    name: "Pawan",
-    Id : 32,
-    gender: "male"
-}
-console.log(obj);
+// let obj = {
+//     name: "Pawan",
+//     Id : 32,
+//     gender: "male"
+// }
+// console.log(obj);
 
- let fb = {
-    userid : "pawan18",
-    password: "pawanXYZ",
-    "Account number" : 221221
- }
- console.log(fb["Account number"]);
-//second method for creating obj
- const person = new Object();
- person.name = "Pawan";
- person.age = 80;
- person.gender = "male";
- console.log(person);
- //delete
- delete person.age;
- console.log(person);
+//  let fb = {
+//     userid : "pawan18",
+//     password: "pawanXYZ",
+//     "Account number" : 221221
+//  }
+//  console.log(fb["Account number"]);
+// //second method for creating obj
+//  const person = new Object();
+//  person.name = "Pawan";
+//  person.age = 80;
+//  person.gender = "male";
+//  console.log(person);
+//  //delete
+//  delete person.age;
+//  console.log(person);
  
- //third method
- class People{
-    constructor(name, age, gender){
-        this.name = name;
-        this.age = age;
-        this .gender = gender;
+//  //third method
+//  class People{
+//     constructor(name, age, gender){
+//         this.name = name;
+//         this.age = age;
+//         this .gender = gender;
 
-    }
- }
+//     }
+//  }
 
- let per1 = new People("Pawan", 20,"male");
- console.log(per1);
+//  let per1 = new People("Pawan", 20,"male");
+//  console.log(per1);
+
+                         //common method
+
+ let obj ={
+    name : "pawan",
+    age : 30,
+    account_balence :354525,
+    gender : "male"
+ };
+ 
+ const arr = Object.values(obj);
+ console.log(arr);
+
+ const arr2 = Object.entries(obj);
+ console.log(arr2);
+
