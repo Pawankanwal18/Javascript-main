4746c9a71e64a91fea9fb9d8b7ecf0e113bfa1ea
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-23 08:38:46 +0530

    added

--- day09.js
+++ day09.js
@@ -1,32 +1,34 @@
-                   //object types
+//                    //object types
 
-let obj1 = {
-    a:1,
-    b:2
-}
-let obj2 = obj1;
-obj2.a =10;
-console.log(obj2, obj1);
+// let obj1 = {
+//     a:1,
+//     b:2
+// }
+// let obj2 = obj1;
+// obj2.a =10;
+// console.log(obj2, obj1);
 
-//deep copy  
-//structureclone is must use and importent
+// //deep copy  
+// //structureclone is must use and importent
 
-let obj3 = structuredClone(obj1);
-obj3.a =20;
-console.log(obj3, obj1); 
-//nested object 
-const user ={
-    name: "pawan",
-    balance: 420,
-    address:
-{
-    pincode: 263632,
-    city:"xyz"
-}
-}   
-//console.log(user.address.pincode);
+// let obj3 = structuredClone(obj1);
+// obj3.a =20;
+// console.log(obj3, obj1); 
+// //nested object 
+// const user ={
+//     name: "pawan",
+//     balance: 420,
+//     address:
+// {
+//     pincode: 263632,
+//     city:"xyz"
+// }
+// }   
+// //console.log(user.address.pincode);
 
-const user2 = Object.assign({},user);
-console.log(user2);
-user2.address.pincode = 78473;
-console.log(user.address.pincode);
+// const user2 = Object.assign({},user);
+// console.log(user2);
+// user2.address.pincode = 78473;
+// console.log(user.address.pincode);
+
+//destructuring of an object
