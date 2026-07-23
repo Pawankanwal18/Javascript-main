85469fd70b7e434b05ee6fd78d31dc1bba13f161
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-23 08:22:28 +0530

    added

--- day09.js
+++ day09.js
@@ -1,1 +1,15 @@
-                   
+                   //object types
+
+let obj1 = {
+    a:1,
+    b:2
+}
+let obj2 = obj1;
+obj2.a =10;
+console.log(obj2, obj1);
+
+//deep copy  
+
+let obj3 = structuredClone(obj1);
+obj3.a =20;
+console.log(obj3, obj1); 
