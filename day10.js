2d3800ac2c7cc6f957535911f39c6cfbac95b5b1
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-23 10:22:25 +0530

    added

--- day10.js
+++ day10.js
@@ -1,13 +1,27 @@
-                            //Prototype/prototypechain
+                            //Prototype/prototypechain also  understand by just like inheritance
 
-  let obj ={
-    name: "pawan",
-    age : 33,
-    addres : {
-        pincode: 232313,
-        city: "kolkata",
-    }
-};
 
- console.log(obj.toString());
+//   let obj ={
+//     name: "pawan",
+//     age : 33,
+//     addres : {
+//         pincode: 232313,
+//         city: "kolkata",
+//     }
+// };
 
+//  console.log(obj.toString());
+
+let user1 = {
+    name: "rohan",
+    age : 34,
+}
+
+
+let user2 = {
+    amount: 32,
+    money :69
+}
+
+user2.__proto__ = user1;
+console.log(user2.name);
