64ea3e96b9f322b31ed4bf8eae7b0fd0d71d0358
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-23 13:39:13 +0530

    added

--- day11.js
+++ day11.js
@@ -1,7 +1,13 @@
               //function
 
-function hello(){
-    console.log("kase ho")
-    console.log(32)
-}
-hello();
+// function hello(){
+//     console.log("kase ho")
+//     console.log(32)
+// }
+// hello();
+
+function sum(number1,number2){
+    console.log(number1+number2)
+};
+sum(2,3);
+sum(10,320);
