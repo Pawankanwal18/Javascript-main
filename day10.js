299982f3cc7491b7e77ff444b388a8c7f6ab6899
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-23 10:05:23 +0530

    added

--- day10.js
+++ day10.js
@@ -1,4 +1,13 @@
                             //Prototype/prototypechain
 
-  
+  let obj ={
+    name: "pawan",
+    age : 33,
+    addres : {
+        pincode: 232313,
+        city: "kolkata",
+    }
+};
+
+ console.log(obj.toString());
 
