38c8ec916d041e856fc45749fd1c528b0dd9acc3
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-23 08:25:31 +0530

    added

--- day09.js
+++ day09.js
@@ -13,3 +13,14 @@ console.log(obj2, obj1);
 let obj3 = structuredClone(obj1);
 obj3.a =20;
 console.log(obj3, obj1); 
+//nested object 
+const user ={
+    name: "pawan",
+    balance: 420,
+    address:
+{
+    pincode: 263632,
+    city:"xyz"
+}
+}
+console.log(user.address.pincode);
