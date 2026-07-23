0c28387c05e0a53fcd68e65f408bd0e576a20df3
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-23 10:34:36 +0530

    addded

--- day10.js
+++ day10.js
@@ -28,3 +28,8 @@ console.log(user2.name);
 
 //arr prototype__prototype is s object 
 //object prototype is same simple words duble prototype is a object and triple object is a null and thats called a protocotype chainning
+
+let arr =[10,20,30,43]
+console.log(arr.__proto__ == Array.prototype)
+console.log(arr.__proto__.__proto__==Object.prototype)
+console.log(arr.__proto__.__proto__.__proto__ == null)
