c6eea494aeb2b9d78dca6e498a7742f1cb2f61ba
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-24 10:17:23 +0530

    added

--- day13.js
+++ day13.js
@@ -7,9 +7,18 @@ let obj ={
     gender : "male"
 };
 
-for(let key in obj)
-{
-    console.log(key,obj[key]);
+// for(let key in obj)
+// {
+//     console.log(key,obj[key]);
 
-}
+// }
  
+let obj= Object.create(obj);
+obj2.money =420;
+obj2.id = "roh";
+// console.log(obj2);
+console.log(Object.keys(obj2));
+for(let key in obj2)
+{
+    console.log(key);
+}
