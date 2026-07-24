b3cd687581063f2c1e716c0ac563e72010f8becb
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-24 06:45:11 +0530

    added

--- day12.js
+++ day12.js
@@ -2,12 +2,46 @@
 
 
 //if else 
-    let age = 30;
-if(age>=18)
-{
-    console.log("eligable for the vote");
+//     let age = 30;
+// if(age>=18)
+// {
+//     console.log("eligable for the vote");
 
-}
-else{
-    console.log("not eligable ");
+// }
+// else{
+//     console.log("not eligable ");
+// }
+
+
+// else if
+
+// multiple condtion : switch 
+
+switch(new Date().getDay())
+{
+    case 0:
+        console.log("sunday");
+    break;
+   case 1:
+   console.log("Monday");
+    break;
+   case 2:
+   console.log("tuesday");
+   break;
+   case 3:
+   console.log("wednesday");
+   break;
+   case 4:
+    console.log("thusday");
+    break;
+    case 4:
+    console.log("friday");
+    break;
+    case 5:
+    console.log("saturday");
+    break;
+    default:
+        console.log("not a valide date");
+
+
 }
