ccb545fb54f71844f8b8ca517b13343b6972fa68
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-24 18:23:32 +0530

    used

--- day13.js
+++ day13.js
@@ -56,22 +56,22 @@
 //inherit hoke bhe koi bhe property or key aatu hai , uska enumrable true hoga to uska bhe access hoga 
 
 
-const customer1={
-    name : "pawan",
-    age :23,
-    account_number :123,
-    balance : 20000
-}
+// const customer1={
+//     name : "pawan",
+//     age :23,
+//     account_number :123,
+//     balance : 20000
+// }
 
 
-let customer2= Object.create(customer1);
-customer2.city = "haridwar";
-customer2.place = "delhi";
+// let customer2= Object.create(customer1);
+// customer2.city = "haridwar";
+// customer2.place = "delhi";
   
 
 
-Object.defineProperty(customer1 ,"name",{
-    enumerable: false,
-})
-for(let key in customer2)
-    console.log(key);
+// Object.defineProperty(customer1 ,"name",{
+//     enumerable: false,
+// })
+// for(let key in customer2)
+//     console.log(key);
