b98b4507572531e325c44676a38d3867384cb4d1
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-24 10:11:45 +0530

    added

---
+++ day13.js
@@ -0,0 +1,15 @@
+                          //advance  loops
+
+let obj ={
+    name : "pawan",
+    age : 32,
+    city : "rampur",
+    gender : "male"
+};
+
+for(let key in obj)
+{
+    console.log(key,obj[key]);
+
+}
+ 
