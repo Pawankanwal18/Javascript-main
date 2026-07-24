8676ff4efd5284a1ff334294822ebfa74a1b6dac
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-24 06:45:47 +0530

    changed

--- day12.js
+++ day12.js
@@ -17,31 +17,31 @@
 
 // multiple condtion : switch 
 
-switch(new Date().getDay())
-{
-    case 0:
-        console.log("sunday");
-    break;
-   case 1:
-   console.log("Monday");
-    break;
-   case 2:
-   console.log("tuesday");
-   break;
-   case 3:
-   console.log("wednesday");
-   break;
-   case 4:
-    console.log("thusday");
-    break;
-    case 4:
-    console.log("friday");
-    break;
-    case 5:
-    console.log("saturday");
-    break;
-    default:
-        console.log("not a valide date");
+// switch(new Date().getDay())
+// {
+//     case 0:
+//         console.log("sunday");
+//     break;
+//    case 1:
+//    console.log("Monday");
+//     break;
+//    case 2:
+//    console.log("tuesday");
+//    break;
+//    case 3:
+//    console.log("wednesday");
+//    break;
+//    case 4:
+//     console.log("thusday");
+//     break;
+//     case 4:
+//     console.log("friday");
+//     break;
+//     case 5:
+//     console.log("saturday");
+//     break;
+//     default:
+//         console.log("not a valide date");
 
 
-}
+// }
