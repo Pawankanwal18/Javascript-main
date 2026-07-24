1ae212a60659d0167a2a18f69f0c23a1ad4cbfaa
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-24 19:04:43 +0530

    added

--- day14.js
+++ day14.js
@@ -1,20 +1,33 @@
-//for of loop to accesing the array  element
+// //for of loop to accesing the array  element
 
-const arr =[10,20,30,40];
-for(let value of arr)
-{
-    console.log(value);
-}
+// const arr =[10,20,30,40];
+// for(let value of arr)
+// {
+//     console.log(value);
+// }
 
-//dont't use for of loop in obj
+// //dont't use for of loop in obj
 
-const obj ={
-    name : "rosni",
-    age : 19,
-    gender : "female"
-}
-for(let value of obj){
-    console.log(value);
-}
+// const obj ={
+//     name : "rosni",
+//     age : 19,
+//     gender : "female"
+// }
+// for(let value of obj){
+//     console.log(value);
+// }
+
  //callback function ,passing an argument(refrence) within any function is called a callback function
+
+ function names(fun){
+    console.log("hello i am name ");
+    fun();
+ }
+
+ function meet(){
+    console.log("my name is callback function");
+
+ }
+
+ names(meet); 
  
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,18 @@
-console.log(customer);
+//for of loop to accesing the array  element
+
+// const arr =[10,20,30,40];
+// for(let value of arr)
+// {
+//     console.log(value);
+// }
+
+// //dont't use for of loop in obj
+
+// const obj ={
+//     name : "rosni",
+//     age : 19,
+//     gender : "female"
+// }
+// for(let value of obj){
+//     console.log(value);
+// }
