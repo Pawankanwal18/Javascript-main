61ec221433acc98f4a80da1cab722234590b7501
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-25 10:26:43 +0530

    new code is return

--- day15.js
+++ day15.js
@@ -1,7 +1,19 @@
                    //reduce
 
-const arr = [10,29,32,43,2];
+// const arr = [10,29,32,43,2];
 
-const result = arr.reduce((acc, curr) => acc+curr, 0);
+// const result = arr.reduce((acc, curr) => acc+curr, 0);
+
+// console.log(result);
+
+let arr= ["orenge", "apple","orenge", "apple","orenge", "apple","orenge"];
+
+const result = arr.reduce((acc,curr)=>{
+    if(acc.hasOwnProperty(curr))
+        acc[curr]++;
+    else
+        acc[curr]=1;
+    return acc;
+},{})
 
 console.log(result);
