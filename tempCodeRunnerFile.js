7db790849abcd69123a2876b40a3d5cab15a7617
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-26 09:23:30 +0530

    added

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,5 +1,6 @@
-let set1 =new Set([10,20,30,40,50]);
-let set2 = new Set([30,50,60,90]);
+let meet = function(){
+//     console.log("hello kase ho")
+// }
 
-let set3 =new set([...set1,...set2]);
-console.log(set3);  
+// meet();
+ 
