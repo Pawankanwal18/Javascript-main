1b62e8c098ff26a610337e7ce5af12a025050d9a
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-26 11:35:30 +0530

    new changes are created

--- day16.js
+++ day16.js
@@ -29,16 +29,16 @@
 //in output {} empty
 
 
-let obj = {
-    name : "rohit",
-    age : 23,
-    greet: function(){
+// let obj = {
+//     name : "rohit",
+//     age : 23,
+//     greet: function(){
        
-    let ab =()=>{
-        console.log(this);
-    };
-    ab();
-}
-}
+//     let ab =()=>{
+//         console.log(this);
+//     };
+//     ab();
+// }
+// }
 
-obj.greet();
+// obj.greet();
