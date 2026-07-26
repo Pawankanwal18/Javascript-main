7695ed1567bbf85edec9b3d0fdfa7971cdd5f01d
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-26 08:49:22 +0530

    added

---
+++ day16.js
@@ -0,0 +1,13 @@
+// alwayes first memory allocation and afer that code part will be excuted
+let meet = function(){
+    console.log("hello kase ho")
+}
+
+meet();
+ 
+
+//memory allocation:
+//meet: function assigne ho jayega
+//var = undefine
+
+//
