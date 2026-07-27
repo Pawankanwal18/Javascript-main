a9c0dc75bc3f8fc771a11a4268c260db48b4038d
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-27 11:15:33 +0530

    completed the project

--- miniproject.js
+++ miniproject.js
@@ -1,9 +1,17 @@
-const time = document.getElementById('root');
+function timing(){
+const timer = document.getElementById('root');
 const now = new Date();
-const IndianTime = now.toLocaleTimeString();
-time.innerHTML = IndianTime;
+const IndianTime = now.toLocaleTimeString("en-us",{
+    hour12:true
+});
+timer.innerHTML = IndianTime;
+}
 
-time.style.fontSize = "120px";
-time.style.display ="flex";
-time.style.justifyContent = "center";
-time.style.height ="100vh";
+ setInterval(timing,1000);
+
+const timer = document.getElementById('root');
+timer.style.fontSize = "100px";
+timer.style.display ="flex";
+timer.style.justifyContent = "center";
+timer.style.height ="100vh";
+timer.style.alignItems ="center";
