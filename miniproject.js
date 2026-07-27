3acc1270fe67e15e97066ec44a66e2bf4d994fe4
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-27 11:48:34 +0530

    new timmer project is added

--- miniproject.js
+++ miniproject.js
@@ -1,17 +1,22 @@
-function timing(){
-const timer = document.getElementById('root');
-const now = new Date();
-const IndianTime = now.toLocaleTimeString("en-us",{
-    hour12:true
-});
-timer.innerHTML = IndianTime;
-}
+// function timing(){
+// const timer = document.getElementById('root');
+// const now = new Date();
+// const IndianTime = now.toLocaleTimeString("en-us",{
+//     hour12:true
+// });
+// timer.innerHTML = IndianTime;
+// }
 
- setInterval(timing,1000);
+//  setInterval(timing,1000);
 
-const timer = document.getElementById('root');
-timer.style.fontSize = "100px";
-timer.style.display ="flex";
-timer.style.justifyContent = "center";
-timer.style.height ="100vh";
-timer.style.alignItems ="center";
+// const timer = document.getElementById('root');
+// timer.style.fontSize = "100px";
+// timer.style.display ="flex";
+// timer.style.justifyContent = "center";
+// timer.style.height ="100vh";
+// timer.style.alignItems ="center";
+
+const id = document.querySelector('#first');
+id.innerHTML ="Hello Money";
+
+const id2 = document.querySelector('.header2');
