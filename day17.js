3ac4b95fcad8ac8784d064c251236d1f97b59e30
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-29 10:53:47 +0530

    events

--- day17.js
+++ day17.js
@@ -1,15 +1,19 @@
-                 //Events
- //mouse event
+//                  //Events
+//  //mouse event
 
-1: MOUSEOVER
-2: mousemove
-3: click
-4: dblclick
+// 1: MOUSEOVER
+// 2: mousemove
+// 3: click
+// 4: dblclick
 
-//<!---keybord event--->
-1: keydown
-2: keyup
+// //<!---keybord event--->
+// 1: keydown
+// 2: keyup
 
-//<!---Event OBject--->
-1 : event
-2 :event.target
+// //<!---Event OBject--->
+// 1 : event
+// 2 :event.target
+// 3: event.type
+// 4: event.key
+// 5:event.clientX
+// 6: event.clientY
