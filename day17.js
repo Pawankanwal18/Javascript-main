754f8421770f14cb630a23b5281d160aae9dded8
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-07-29 10:33:56 +0530

    changeses are commited

--- day17.js
+++ day17.js
@@ -1,1 +1,15 @@
                  //Events
+ //mouse event
+
+1: MOUSEOVER
+2: mousemove
+3: click
+4: dblclick
+
+//<!---keybord event--->
+1: keydown
+2: keyup
+
+//<!---Event OBject--->
+1 : event
+2 :event.target
