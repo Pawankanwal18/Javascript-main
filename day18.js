866f7ca2e4260c8710322d6286144c9b4931ffe4
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-08-05 17:32:54 +0530

    code is added

--- day18.js
+++ day18.js
@@ -2,7 +2,19 @@
 function fetchuser(){
     console.log("fetching user data from the database")
     setTimeout(()=>{
-        console.log("user data fetched")
+        console.log("user data fetched");
+        const name = "pawan";
+        greet(name);
+        meet(name);
     }, 2000)
 }
  
+function greet(name){
+    console.log(`hello ${name}`);
+}
+
+function meet(name){
+    console.log(`hello ${name}, nice to meet you!`);
+}
+
+fetchuser();
