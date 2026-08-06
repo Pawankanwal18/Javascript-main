4adac42943f49f9e0c5835577597626adc60f547
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-08-06 20:20:25 +0530

    code is added

--- day19.js
+++ day19.js
@@ -1,2 +1,19 @@
     //   call back hell
-    
+    function pizzaOrder(){
+        console.log("placing order for pizza");
+
+        setTimeout(()=>{
+            console.log("order is placed for pizza");
+        } ,2000)
+    }
+    pizzaOrder();
+
+    function pizzaPrepare(){
+        console.log("preparing pizza");
+
+        setTimeout(()=>{
+            console.log("pizza is prepared");
+        } ,5000)
+    }
+
+    pizzaPrepare();
