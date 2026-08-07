7db8c655a1beb14a8ebd3d5a85d4b930eed7a11b
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-08-07 09:30:41 +0530

    code is comment out

--- day19.js
+++ day19.js
@@ -1,35 +1,35 @@
-    //   call back hell
-    function pizzaOrder(Callback){
-        console.log("placing order for pizza");
+//     //   call back hell
+//     function pizzaOrder(Callback){
+//         console.log("placing order for pizza");
 
-        setTimeout(()=>{
-            console.log("order is placed for pizza");
-            Callback();
-        } ,2000)
-    }
+//         setTimeout(()=>{
+//             console.log("order is placed for pizza");
+//             Callback();
+//         } ,2000)
+//     }
    
 
-    function pizzaPrepare(callback){
-        console.log("preparing pizza");
+//     function pizzaPrepare(callback){
+//         console.log("preparing pizza");
 
-        setTimeout(()=>{
-            console.log("pizza is prepared");
-            callback()
-        } ,5000)
-    }
+//         setTimeout(()=>{
+//             console.log("pizza is prepared");
+//             callback()
+//         } ,5000)
+//     }
 
    
-    function pizzaDeliver(){
-        console.log("delivering pizza");
+//     function pizzaDeliver(){
+//         console.log("delivering pizza");
 
-        setTimeout(()=>{
-            console.log("pizza is delivered succesfully");
-        } ,3000)
-    }
+//         setTimeout(()=>{
+//             console.log("pizza is delivered succesfully");
+//         } ,3000)
+//     }
 
 
-pizzaOrder(()=>{
-    pizzaPrepare(()=>{
-        pizzaDeliver();
-    });
-}); 
+// pizzaOrder(()=>{
+//     pizzaPrepare(()=>{
+//         pizzaDeliver();
+//     });
+// });  
