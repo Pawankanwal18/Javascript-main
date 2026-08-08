14f5d9bceb024e70fcdfb0eb14fa4770e92a8be2
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-08-08 19:42:48 +0530

    promise code is added

--- day20.js
+++ day20.js
@@ -1,5 +1,14 @@
  //wether
- const obj =fetch(`http://api.weatherapi.com/v1/current.json?key=d1cf592c162e45739d7121722260808&q=London&aqi=yes`)
-obj.then((data)=>{
-    console.log(data);
-});
+//  const obj =fetch(`http://api.weatherapi.com/v1/current.json?key=d1cf592c162e45739d7121722260808&q=London&aqi=yes`)
+// obj.then((data)=>{
+//     console.log(data);
+// });
+
+
+const Promises = fetch(`http://api.weatherapi.com/v1/current.json?key=d1cf592c162e45739d7121722260808&q=London&aqi=yes`)
+
+ Promises.then((response)=>{
+    console.log(response);
+}).catch((error)=>{
+    console.log(error);
+})
