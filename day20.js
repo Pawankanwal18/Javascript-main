ea64ba36b56a2799bf4014540af0620c572f27df
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-08-08 19:52:08 +0530

    prmosies chaining

--- day20.js
+++ day20.js
@@ -7,8 +7,18 @@
 
 const Promises = fetch(`http://api.weatherapi.com/v1/current.json?key=d1cf592c162e45739d7121722260808&q=London&aqi=yes`)
 
- Promises.then((response)=>{
-    console.log(response);
-}).catch((error)=>{
-    console.log(error);
+
+
+//  Promises.then((response)=>{
+//     console.log(response);
+// }).catch((error)=>{
+//     console.log(error);
+// })
+
+
+//promises chaining 
+Promises.then((response)=>{
+    return response.json();
+}).then((data)=>{
+    console.log(data);
 })
