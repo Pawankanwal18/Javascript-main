0081ff8a6fe4c80cd82d0b41cbf1d8165624478b
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-08-08 18:43:14 +0530

    optimized the code

--- day20.js
+++ day20.js
@@ -1,6 +1,5 @@
  //wether
  const obj =fetch(`http://api.weatherapi.com/v1/current.json?key=d1cf592c162e45739d7121722260808&q=London&aqi=yes`)
-setTimeout(()=>{
-   console.log(obj);
-},2000)
-
+obj.then((data)=>{
+    console.log(data);
+});
