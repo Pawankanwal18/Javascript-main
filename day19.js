ed7bc1a18d85daefdd6e3e439bfbf27a77aabc0e
Pawankanwal18 <kanwalpawan72@gmail.com>
2026-08-10 17:02:03 +0530

    added

--- day19.js
+++ day19.js
@@ -1,38 +1,45 @@
-//     //   call back hell
-//     function pizzaOrder(Callback){
-//         console.log("placing order for pizza");
+    // callback hell converted to async/await
+    function pizzaOrder(){
+        return new Promise((resolve) => {
+            console.log("talking for pizza order");
 
-//         setTimeout(()=>{
-//             console.log("order is placed for pizza");
-//             Callback();
-//         } ,2000)
-//     }
-   
+            setTimeout(() => {
+                console.log("order is placed for pizza");
+                resolve();
+            }, 2000)
+        });
+    }
 
-//     function pizzaPrepare(callback){
-//         console.log("preparing pizza");
+    function pizzaPrepare(){
+        return new Promise((resolve) => {
+            console.log("preparing pizza");
 
-//         setTimeout(()=>{
-//             console.log("pizza is prepared");
-//             callback()
-//         } ,5000)
-//     }
+            setTimeout(() => {
+                console.log("pizza is prepared");
+                resolve();
+            }, 5000)
+        });
+    }
 
-   
-//     function pizzaDeliver(){
-//         console.log("delivering pizza");
+    function pizzaDeliver(){
+        return new Promise((resolve) => {
+            console.log("delivering pizza");
 
-//         setTimeout(()=>{
-//             console.log("pizza is delivered succesfully");
-//         } ,3000)
-//     }
+            setTimeout(() => {
+                console.log("pizza is delivered succesfully");
+                resolve();
+            }, 3000)
+        });
+    }
 
+    async function greet(){
+        await pizzaOrder();
+        await pizzaPrepare();
+        await pizzaDeliver();
+        console.log("Food delivered successfully");
+    }
 
-// pizzaOrder(()=>{
-//     pizzaPrepare(()=>{
-//         pizzaDeliver();
-//     });
-// });  
+    greet();
 
 
 //java is a single threaded syncronous language:
@@ -61,5 +68,3 @@
 
  //web api has set timeout, set interval, fetch, dom, console there all are the part of the web api 
  //web api is present in the browser of your computer ,event listione are also the part of the web api 
-
- 
