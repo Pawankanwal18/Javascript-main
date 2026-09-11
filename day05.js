6362621a5951da89c20132035f3a089181396aa9
Pawan Kanwal <kanwalpawan72@gmail.com>
2026-09-11 21:57:44 +0530

    added

--- day05.js
+++ day05.js
@@ -2,10 +2,10 @@ let num1 = 231;
 let num2 = new Number(231);
 let num3 = new Number(231);
 
-console.log(num1==num2);
-console.log(num2==num3);
+console.log(num1 == num2);
+console.log(num2 == num3);
 
-let num =231.68;
+let num = 231.68;
 console.log(num.toFixed(2));
 console.log(num.toPrecision(4));
 console.log(num.toExponential(2));
@@ -24,14 +24,14 @@ let num = 23.1;
 console.log(Math.floor(num));
 console.log(Math.ceil(num));
 
-console.log(Math.floor(Math.random()*10));
+console.log(Math.floor(Math.random() * 10));
 //genrate the value from 1 to 9
 
 //genrate the value between min=40 , to max = 50;
 //formula 
 //console.log(Math.floor(Math.random()*(max-min+1)+min));
 
-console.log(Math.floor (Math.random()*11)+40);
+console.log(Math.floor(Math.random() * 11) + 40);
 
 //console.log(Math.floor(Math.random()*10)); find number from 0 to 9 and for 0 to 10 multiply by 11
 
@@ -41,4 +41,6 @@ console.log(Math.floor (Math.random()*11)+40);
 // console.log(Math.floor(Math.random()*(10+1)+3));
 
 //ludo 1-6
-console.log(Math.floor(Math.random()*(6-1+1)+1));
+console.log(Math.floor(Math.random() * (6 - 1 + 1) + 1));
+
+//can find any value
